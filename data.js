0468fc94f7674389b6da5ca158c6c8d65ee6107f
Anna <krolikowska.ann@gmail.com>
2023-07-12 16:40:14 +0200

    added data base

---
+++ data.js
@@ -0,0 +1,94 @@
+const expenses = [
+    {
+      id: 1,
+      name: "cinema",
+      price: 40,
+      currency: "PLN",
+      icon: "./icons/icon-1.png",
+      category: "entertainment",
+      date: "2022-07-16"
+    },
+    {
+      id: 2,
+      name: "bread",
+      price: 8,
+      currency: "PLN",
+      icon: "./icons/icon-4.png",
+      category: "food",
+      date: "2022-08-03"
+    },  
+    {
+      id: 3,
+      name: "fuel",
+      price: 320,
+      currency: "PLN",
+      icon: "./icons/icon-5.png",
+      category: "car",
+      date: "2022-08-14"
+    },
+    {
+      id: 4,
+      name: "pet food",
+      price: 21,
+      currency: "PLN",
+      icon: "./icons/icon-2.png",
+      category: "animals",
+      date: "2022-08-03"
+    },
+    {
+      id: 5,
+      name: "pizza",
+      price: 8,
+      currency: "PLN",
+      icon: "./icons/icon-4.png",
+      category: "food",
+      date: "2022-09-01"
+    },
+    {
+      id: 6,
+      name: "fuel",
+      price: 140,
+      currency: "PLN",
+      icon: "./icons/icon-5.png",
+      category: "car",
+      date: "2022-09-01"
+    },
+    {
+      id: 7,
+      name: "butter",
+      price: 6,
+      currency: "PLN",
+      icon: "./icons/icon-4.png",
+      category: "food",
+      date: "2022-09-10"
+    },
+    {
+      id: 8,
+      name: "zoo",
+      price: 40,
+      currency: "PLN",
+      icon: "./icons/icon-1.png",
+      category: "entertainment",
+      date: "2022-07-22"
+    },
+    {
+      id: 9,
+      name: "vet",
+      price: 200,
+      currency: "PLN",
+      icon: "./icons/icon-2.png",
+      category: "animals",
+      date: "2022-07-03"
+    },
+    {
+      id: 10,
+      name: "hotel",
+      price: 250,
+      currency: "PLN",
+      icon: "./icons/icon-1.png",
+      category: "entertainment",
+      date: "2022-08-03"
+    },
+];
+  
+export default expenses;
